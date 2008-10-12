9a943db604b48b367fbc793fe715536bf7963c35
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2008-10-12 21:27:10 +0000

    ajout du test de la fonction echange

---
+++ POSIX/S2/temp/test.c
@@ -0,0 +1,19 @@
+#define _POSIX_SOURCE 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void echange(char * dest, char * src, int taille){
+  strncpy(dest, src, taille);
+  dest[taille-1]='o';
+  printf("la valeur de argv[1]: %s\n",dest);
+}
+
+int main(int argc,char *argv[]){
+  char *dest ;
+  int taille = strlen(argv[1]);
+  dest = malloc(taille+1 * sizeof(char)); /* il faut oublier le \0 */
+  echange (dest, argv[1], taille+1); /* on prend en compte*/
+  printf("la valeur de argv[1]: %s\n",dest);
+  return 0;
+}
