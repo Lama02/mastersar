a22cab300ecce53b681b11559047f3bae0c01e8b
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2008-10-12 23:07:23 +0000

    implementation de echange dans l exo3-1

--- POSIX/S2/Semaine_2/src_C/exo3-1.c
+++ POSIX/S2/Semaine_2/src_C/exo3-1.c
@@ -5,6 +5,7 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+#include<string.h>
 
 /* le path vers gcc */
 #define CC_PATH "/usr/bin/gcc"
@@ -14,10 +15,44 @@
 */
 #define CFLAGS  "-Wall"
  
+
+/* 
+   si src=test 
+   dest deviendra teso 
+*/
+void echange(char * dest, char * src, int taille){
+  strncpy(dest, src, taille);
+  dest[taille-1]='o';
+}
+
+
+
 int main(int argc, char * argv[]){
-    
+  
+  /* contiendra les nom des fichiers objets */
+  char * tab_fils[]; 
+  
+  /* contiendra le nom du fichier d objet en cours de compilation */
+  /* cette variable sera ecrase a chaque fois qu un nouveau fichier */
+  /* source est traite */
+  char * dest; 
+  
   pid_t fils;
   int i=0;
+  /* taille du nom du fichier */
+  int taille; 
+  /* 
+     faire les substitutions .c -> .o et mettre le 
+     dans un tableau
+  */
+  while (i < argc -1){
+    taille = strlen(argv[1]);
+    dest = malloc(taille+1 * sizeof(char)); /* il faut oublier le \0 */
+    echange (dest, argv[1], taille); 
+    
+    
+    i++;
+  }
   printf("=== Compilation des sources ===\n");
   printf("argc = %d\n", argc);
   while (i < argc -1){
@@ -25,6 +60,11 @@ int main(int argc, char * argv[]){
       /* si je suis dans le fils */
       /* je lance la compilation du fichier */
       printf("Compilation du fichier %s ...\n",argv[i+1]); /* le backslach sera consomme par execl */
+      
+      /* stocker le nom du fichier dans un tab_fils */
+      dest = malloc(taille+1 * sizeof(char)); /* il faut oublier le \0 */
+      echange (dest, argv[1], taille); 
+
       execl(CC_PATH, CC, CFLAGS, "-c", argv[i+1], NULL);
       /* Si on revient ici alors execl s est mal executee */
       printf("Erreur: execl\n");
@@ -34,8 +74,6 @@ int main(int argc, char * argv[]){
       i++;
     }
   }
-
-  
   
   /* on a attend la mort des tous les fils */
   i=0;
@@ -44,6 +82,7 @@ int main(int argc, char * argv[]){
     i++;
   }
   printf("=== Edition des liens ... ===\n");
+  
   return 0;
 
 }
