d8073bf351b98fbc0873cbd813a01f723c74001e
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2008-10-13 00:05:36 +0000

    la génération des objet marche tres bien meme avec des sources de type PATH/src.c ceci génère le fichier objet PPATH/src.o

--- POSIX/S2/Semaine_2/src_C/exo3-1.c
+++ POSIX/S2/Semaine_2/src_C/exo3-1.c
@@ -30,7 +30,7 @@ void echange(char * dest, char * src, int taille){
 int main(int argc, char * argv[]){
   
   /* contiendra les nom des fichiers objets */
-  char * tab_fils[]; 
+  char * tab_fils[argc-1]; 
   
   /* contiendra le nom du fichier d objet en cours de compilation */
   /* cette variable sera ecrase a chaque fois qu un nouveau fichier */
@@ -41,31 +41,32 @@ int main(int argc, char * argv[]){
   int i=0;
   /* taille du nom du fichier */
   int taille; 
+  
   /* 
-     faire les substitutions .c -> .o et mettre le 
-     dans un tableau
+     faire les substitutions .c -> .o et mettre les 
+     resultats dans un tab_fils
   */
   while (i < argc -1){
-    taille = strlen(argv[1]);
-    dest = malloc(taille+1 * sizeof(char)); /* il faut oublier le \0 */
-    echange (dest, argv[1], taille); 
-    
-    
+    taille = strlen(argv[i+1]);
+    dest = malloc(taille+1 * sizeof(char)); /* il faut pas oublier le \0 */
+    /* transformer src.c en src.o */
+    echange (dest, argv[i+1], taille); 
+    /* sauvegarder la reference vers le nom du fichier objet*/
+    /* car elle sera perdu a la prochaine iteration */
+    tab_fils[i]=dest; 
     i++;
   }
+  
   printf("=== Compilation des sources ===\n");
-  printf("argc = %d\n", argc);
+  i=0;
   while (i < argc -1){
     if ( (fils=fork()) == 0 ){
+      printf("je suis la i=%d\n",i);
       /* si je suis dans le fils */
       /* je lance la compilation du fichier */
-      printf("Compilation du fichier %s ...\n",argv[i+1]); /* le backslach sera consomme par execl */
-      
-      /* stocker le nom du fichier dans un tab_fils */
-      dest = malloc(taille+1 * sizeof(char)); /* il faut oublier le \0 */
-      echange (dest, argv[1], taille); 
-
-      execl(CC_PATH, CC, CFLAGS, "-c", argv[i+1], NULL);
+      printf("Generation du fichier %s ...\n",tab_fils[i]); /* le backslach sera consomme par execl */      
+      /* gcc -Wall -o path/toto.o -c path/toto.c */
+      execl(CC_PATH, CC, CFLAGS, "-o", tab_fils[i], "-c", argv[i+1], NULL);
       /* Si on revient ici alors execl s est mal executee */
       printf("Erreur: execl\n");
       exit(1);
@@ -82,6 +83,7 @@ int main(int argc, char * argv[]){
     i++;
   }
   printf("=== Edition des liens ... ===\n");
+  
   
   return 0;
 
