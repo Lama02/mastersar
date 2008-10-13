8b70059c0f2e7d3174d40b4a0ac86c1f2f2ccd44
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2008-10-13 22:51:15 +0000

    un peu de menage

--- POSIX/S2/temp/test.c
+++ POSIX/S2/temp/test.c
@@ -3,15 +3,16 @@
 #include<stdlib.h>
 #include<string.h>
 
+
 void echange(char * dest, char * src, int taille){
   strncpy(dest, src, taille);
-  dest[taille-1]='o';
+  dest[taille-1]='o'; 
 }
 
 int main(int argc,char *argv[]){
   char *dest ;
   int taille = strlen(argv[1]);
-  dest = malloc(taille+1 * sizeof(char)); /* il faut oublier le \0 */
+  dest = malloc(taille+1 * sizeof(char)); /* il faut pas oublier le \0 */
   echange (dest, argv[1], taille); 
   printf("la valeur de argv[1]: %s\n",dest);
   return 0;
