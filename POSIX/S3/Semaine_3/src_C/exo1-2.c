d422c813dd529eb6c9230ad595cd05b2fa591231
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2008-10-13 22:38:38 +0000

    ajout du TME 3 de Posix

---
+++ POSIX/S3/Semaine_3/src_C/exo1-2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define _POSIX_SOURCE 1
+
+
+int main (int argc, char * argv[]){
+  
+  /* definir les signaux bloquees */
+  sigset_t sig_set, old_sig_set;
+  int i;
+
+  sigemptyset(&sig_set);
+  sigaddset(&sig_set, SIGINT);
+  sigaddset(&sig_set, SIGQUIT);
+  
+  /* masquer les signaux definies dans l ensemble sig_set */
+  /* sauvegarder le masque du systeme dans old_sig_set */
+  /* avant de l ecraser avec la valeur de sig_set */
+  sigprocmask(SIG_SETMASK, &sig_set, &old_sig_set);
+  
+  sleep(5);
+  
+  
+  /* lister les signaux pendants */
+  if (sigpending (&sig_set) == -1)
+    return EXIT_FAILURE;
+  
+
+  for (i=0; i<NSIG ; i++){
+    if (sigismember(&sig_set, i) == 1)
+      {
+	if (i == SIGINT)
+	  printf("\nSIGINT\n");
+	else
+	  {
+	  if (i == SIGQUIT)
+	    printf("SIGQUIT\n");
+	  }
+      }
+  }
+  
+  
+  /* effacer le masque des signaux pour SIGINT et SIGQUIT */
+  sigprocmask(SIG_SETMASK, &old_sig_set, NULL);
+  
+  /* cette partie du code ne sera pas traitee dans le cas ou */ 
+  /* ou on avant envoye un des dignaux SIGINT ou SIGQUIT, car */ 
+  /* les signaux masquees seront maintenant pris en compte. */
+  
+  printf("Reprise apres demasquage\n");
+  
+  return EXIT_SUCCESS;
+}
