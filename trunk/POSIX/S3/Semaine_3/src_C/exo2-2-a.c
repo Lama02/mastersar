9ddb9a777b4e28245be963ae435c7a451f90e6db
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2008-10-19 15:04:23 +0000

    la premiere partie de l exo2-2 est faite cad ignorer le signal SIGTERM grace aux sigactions. Adaptation du makefile pour notre TME. Une petite amelioration dans l'exo1-2

---
+++ trunk/POSIX/S3/Semaine_3/src_C/exo2-2-a.c
@@ -0,0 +1,73 @@
+/* exo2-2.c */
+/* Ce programme presente une solution de l'exo 2-2 en */
+/* utilisant les sigations */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define _POSIX_SOURCE 1
+#define SEC 20
+#define NMAX 3 /* nombre maxi de SIGINT necessaire pour arreter le prog */
+
+int n=0; /* nombre de SIGINT recu */
+
+void sig_hand(int sig){
+  switch (sig) {
+  case SIGALRM: printf("%ds se sont ecoulees.\n", SEC); exit(0);
+    /* lorsque SIGTERM est recu le process est reveille mais le signal est */
+    /* en quelque sortes ignore car on fait aucun traitement */
+  case SIGTERM: fprintf(stderr,"Je ne devrais pas voir ce message\n");return;
+  case SIGINT:  if (n<NMAX-1) n++; else exit(0); 
+  default: return;
+  }
+}
+
+
+int main (int argc, char * argv[]){
+  
+  /* definir les signaux bloquees */
+  sigset_t sig_set, old_sig_set;
+  struct sigaction action;
+  
+  
+  /* Signaux a masquee */
+  
+  /* Tous*/
+  sigfillset(&sig_set);
+
+  /* sauf ces trois */
+  sigdelset(&sig_set, SIGINT);
+  sigdelset(&sig_set, SIGALRM);
+  sigdelset(&sig_set, SIGTERM);
+  
+  /* masquer les signaux definies dans l ensemble sig_set */
+  sigprocmask(SIG_SETMASK, &sig_set, &old_sig_set);
+  
+  
+  /* changement de traitement */
+  action.sa_mask = sig_set;
+  action.sa_flags = 0;
+  action.sa_handler = sig_hand;
+
+  /* pour les signaux SIGINT et SIGALRM */
+  /* on applique le traitement definie par */
+  /* la fonction sig_hand */
+  sigaction(SIGINT, &action, NULL);
+  sigaction(SIGALRM, &action, NULL);
+  
+  /*
+   * pour le signal SIGTERM on change le 
+   * traitement. SIGTERM est ignore
+   */
+  action.sa_handler = SIG_IGN;
+  sigaction(SIGTERM, &action, NULL);
+  
+  alarm(SEC);
+  while (1){
+    sigsuspend(&sig_set);
+  }
+
+  return EXIT_SUCCESS;
+}
