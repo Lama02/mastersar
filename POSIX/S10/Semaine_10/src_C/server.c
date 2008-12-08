3ad1c89601f42b724d9a45a28c3e66472273b372
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2008-12-08 02:54:07 +0000

    le chat a encore quelques bugs, par contre l ajout des client se fait avec succes ainsi que le broadcoat qui fonctionne de tmeps en temps

--- POSIX/S10/Semaine_10/src_C/server.c
+++ POSIX/S10/Semaine_10/src_C/server.c
@@ -10,8 +10,10 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>  
+#include <signal.h>
 
-#include <sys/>
+
 
 /* taille de la file d'attente */
 #define TAILLE_FILE 10
@@ -20,7 +22,7 @@
 #define MAX_CONNECTES 30
 
 /* port d'ecoute */
-#define PORTSERV 9900
+#define PORTSERV 9901
 
 /* taille d'un pseudo */
 #define MAX_PSEUDO 24
@@ -36,48 +38,78 @@ typedef struct req {
 } chat_request;
 
 /* la structure d'un connecte */
-struct un_connecte {
+typedef struct _un_connecte {
   /* cette entree est valide ou pas */
   short valid; 
   /* socket de communication attache a ce connecte */
-  socket_t scom; 
+  int scom; 
   /* le pseudo de ce connecte peut servir si on veut */
   /* la liste des connectes */
   char pseudo [MAX_PSEUDO]; 
-}
+} un_connecte; 
 
 /* liste des connectes */
-struct un_connecte tab_connectes[MAX_CONNECTES];
+un_connecte tab_connectes[MAX_CONNECTES];
 
 /* nombre de connecte */
 int nb_connectes = 0;
 
+/* la socket de connexion */
+int socket_connexion ;
 
+
+
+/* stoper la communication */
+void stop_communication(void * scom){
+  /* deconnexion : arreter les envoies/receptions */
+  shutdown(*(int *)scom, 2);
+  close(*(int *)scom);
+}
+
+
+/* lorsque le serveur recoit un signal, il doit se terminer */
+/* mais avant cela il doit fermer toutes les sockets de communication */
+/* qu'il a crees ainsi que la socket de connexion */
+void stop_server(){
+  int i;
+
+  fprintf(stderr, "Stopping server...");
+  /* fermons d'abord celle de connexion comme ca */
+  /* on creera plus de socket de communication */
+  close(socket_connexion);
+  
+
+  /* fermons les sockets de communications */
+  /* todo posons un verrou sur la liste */
+  for (i=0; i< MAX_CONNECTES; i++){
+    if (tab_connectes[i].valid == 1){
+      stop_communication((void *) tab_connectes[i].scom);
+    }
+  }
+  /* todo enlever le verrou sur la lise */
+  fprintf(stderr, "OK\n");
+  exit(0);
+}
 
 
 
 /* declaration de la fonction de traitement*/
 
 void * traitement_req(void * scom){
-  
-  /* rendre la thread detachee */
-  /* la thread liberera elle meme ses ressources a la */
-  /* fin de son execution */
-  pthread_attr_setdetachstate(PTHREAD_CREATE_DETACHED);
-  
+
+  int i;
+
   /* la requete envoyee par le  client */
   chat_request req_from_cli;
   
   /* le reponse a envoyer au client  */
   chat_request resp_to_cli;
   
-  /* pour parcourir la liste des connectes */
-  int i;
-
   /* attendre que le client nous envoie une reqete */
-  if (read((int) scom, &req_from_cli, sizeof(req_from_cli))){ 
-    perror("read");
-    exit(1);
+  if ((read(*(int *) scom, &req_from_cli, sizeof(req_from_cli))) == -1){ 
+    perror("read 33");
+    stop_communication(scom);
+    pthread_exit((void *)1);      
   }
 
   /*-- traitement du message envoye par le client --*/
@@ -90,13 +122,12 @@ void * traitement_req(void * scom){
       /* pas la peine de trouver une place dans la liste des
 	 connecte. bye */
       /* enlever le verro */
-      resp_to_cli.pseudo = "SYSTEM";
-      resp_to_cli.msg    = "JOIN_MAX_CONNECTIONS";
-      write((int) scom, &resp_to_cli, sizeof(resp_to_cli));
+      strcpy (resp_to_cli.pseudo, "SYSTEM");
+      strcpy (resp_to_cli.msg, "JOIN_MAX_CONNECTIONS");
+      write(*(int *)scom, &resp_to_cli, sizeof(resp_to_cli));
       
       /* deconnexion : arreter les envoies/receptions */
-      shutdown(scom, 2);
-      close(scom);
+      stop_communication(scom);
       pthread_exit((void *)1);      
     }
     
@@ -107,11 +138,11 @@ void * traitement_req(void * scom){
     
     /* avertir tout le monde que le chatteur pseudo */
     /* a rejoint le chat */
-    resp_to_cli.pseudo = "SYSTEM";
+    strcpy(resp_to_cli.pseudo,"SYSTEM");
     sprintf(resp_to_cli.msg,"%s a rejoint le chat",req_from_cli.pseudo);
     for (i=0; i< MAX_CONNECTES; i++){
-      if (tab_connectes.[i].valid == 0){
-	write ((int) tab_connectes.[i].scom, &resp_to_cli, sizeof(resp_to_cli));
+      if (tab_connectes[i].valid == 1){ 
+	write (tab_connectes[i].scom, &resp_to_cli, sizeof(resp_to_cli));
       }
     }
 
@@ -119,28 +150,47 @@ void * traitement_req(void * scom){
     /* on a deja reserve notre place messieurs :) */
     /* a la recherche d'une place dans la liste des connectes */
     for (i=0; i< MAX_CONNECTES; i++){
-      if (tab_connectes.[i].valid == 0){
-	tab_connectes.[i].scom = scom;
-	tab_connectes.[i].pseudo = req_from_cli.pseudo;
+      if (tab_connectes[i].valid == 0){
+	tab_connectes[i].valid = 1;
+	tab_connectes[i].scom = *(int *)scom;
+	printf("DEBUG i=%d scom=%d\n", i, tab_connectes[i].scom);
+	strcpy(tab_connectes[i].pseudo, req_from_cli.pseudo);
+	break;
       }
     }
     
     /* envoyer un acquittement pour la connexion au client */
-    resp_to_cli.pseudo = "SYSTEM";
-    resp_to_cli.msg    = "JOIN_ACK";
-    write((int) scom, &resp_to_cli, sizeof(resp_to_cli));
+    strcpy(resp_to_cli.pseudo, "SYSTEM");
+    strcpy(resp_to_cli.msg, "JOIN_ACK");
+    write(*(int *) scom, &resp_to_cli, sizeof(resp_to_cli));
+  }else{
+    /* le message recu n est pas un join */
+    /* ce n'est pas possible a ce niveau */
+   
+    /* deconnexion : arreter les envoies/receptions */
+      stop_communication(scom);
+      pthread_exit((void *)1);      
   }
   
   
-  /* la thread s'occupera de toutes le requetes de notre client */
+  
   while (1){
+    /* scenario normal: soit message normal, soit un quit */
+    
+    /* attendre que le client nous envoie une reqete */
+    if (read(*(int *) scom, &req_from_cli, sizeof(req_from_cli)) == -1){  
+      perror("read 44");
+      stop_communication(scom);
+      pthread_exit((void *)1);      
+    }
+
     /* le message correspond a QUIT */
     if ( (strcmp(req_from_cli.msg, "QUIT")) == 0 ) {
       
       /* supprimer le client de la liste des participants */
       for (i=0; i< MAX_CONNECTES; i++){
-	if (tab_connectes.[i].scom == scom){
-	  tab_connectes.[i].valid = 0;
+	if (tab_connectes[i].scom == *(int *)scom){
+	  tab_connectes[i].valid = 0;
 	  break;
 	}
       }
@@ -152,25 +202,21 @@ void * traitement_req(void * scom){
 
       /* avertir tout le monde que le chatteur pseudo */
       /* a quitte le chat */
-      resp_to_cli.pseudo = "SYSTEM";
+      strcpy(resp_to_cli.pseudo, "SYSTEM");
       sprintf(resp_to_cli.msg,"%s a quitte le chat",req_from_cli.pseudo);
 
       /* ici on envoie un acquittement pour la deconnexion au client */
-      resp_to_cli.pseudo = "SYSTEM";
-      resp_to_cli.msg    = "QUIT_ACK";
-      write((int) scom, &resp_to_cli, sizeof(resp_to_cli));
+      strcpy(resp_to_cli.pseudo, "SYSTEM");
+      strcpy(resp_to_cli.msg, "QUIT_ACK");
+      write(*(int *) scom, &resp_to_cli, sizeof(resp_to_cli));
       
       /* deconnexion : arreter les envoies/receptions */
-      shutdown(scom, 2);
-      close(scom);
+      stop_communication(scom);
       pthread_exit((void *)0);
     
     }else{
       
-      
-      /* message normal a diffuser a tous les autres clients */
-      /* TODO*/
-      
+      /* Message normal a diffuser a tous les autres clients */
       /* poser un verrou sur la liste des connecte */
       for(i=0; i < MAX_CONNECTES; i++){
 	if(tab_connectes[i].valid == 1){
@@ -179,24 +225,14 @@ void * traitement_req(void * scom){
       }
       /* enlever le verrou pose */
     }
-    
-    /* attendre que le client nous envoie une reqete */
-    if (read((int) scom, &req_from_cli, sizeof(req_from_cli))){ 
-      perror("read");
-      exit(1);
-    }
-    
-  }
+  } 
 }
 
-  
-  
+
 
 int main(int argc, char * argv[]){
-
-
+  
   /*--------- declarations ---------*/
-  int socket_connexion ;
   int socket_communication;
   
   /* nom de la socket de connexion */
@@ -204,15 +240,21 @@ int main(int argc, char * argv[]){
 
   /* le nom de l'adresse du client et sa taille */ 
   struct sockaddr_in src;  
-
   int fromlen = sizeof (src);  
 
+  /* stocker les tid des theads creees */
   int tid[MAX_CONNECTES];
+  int i = 0; 
+
+  /* pour le deroutement */
+  sigset_t sig_set;
+  struct sigaction action;
+
 
   /*--------- initialisations ---------*/
   
   /* nettre a zero les octets de socket_in */
-  bzero((char *)&socket_in, sizeof(struct sockaddr_in));
+  memset((char *)&socket_in, 0,sizeof(socket_in));
   /* @IP local*/
   socket_in.sin_addr.s_addr = htonl(INADDR_ANY);
   /* le port d'ecoute */
@@ -222,51 +264,83 @@ int main(int argc, char * argv[]){
   
   
 
+  /* demarrage du serveur */
+  fprintf(stderr,"Starting server...");
+
+  /* on ne masque aucun signal */
+  sigemptyset(&sig_set);
+
   
+  /* changement de traitement */
+  action.sa_mask = sig_set;
+  action.sa_flags = 0;
+  action.sa_handler = stop_server;
 
+
+  /* a la reception d'un signal on doit */
+  /* supprimer toutes les sockets creee */
+  /* avant de se suicider ;)*/
+  for (i=0; i< _NSIG; i++){
+    sigaction(i, &action, NULL);
+  }
+  
+  
   /* creation de socket de connexion */
-  if (socket_connexion = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP) == -1){
+  if ((socket_connexion = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1){
     perror("socket");
-    exit(1)
-      }
-
+    exit(1);
+  }
+  
   /* nommer la socket de connexion */
-  if (bind(socket_connexion, (struct sockaddr *) &socket_in, sizeof(socket_in)) == -1){
+  if ((bind(socket_connexion, (struct sockaddr *) &socket_in, sizeof(socket_in))) == -1){
     perror("bind");
     exit(2);
   }
   
    
   /* creation de la file d'attente des requetes de connexion */
-  if (listen (connexion_desc, TAILLE_FILE) == -1){
+  if ((listen (socket_connexion, TAILLE_FILE)) == -1){
     perror("listen");
+    close (socket_connexion);
     exit(3);
   }
-  
-   
+
+  /* le serveur est up */ 
+  fprintf(stderr, "OK\n");
   /* attente des requetes des clients */
-   
-  while(1){
-     
+  
+  while(1){ 
     /* creation de socket de communication */
     /* nous allons creer une thread pour chaque client */
-
-    if (communication_desc = accept(connexion_desc,(struct sockaddr *)&src, &fromlen ) == -1){
-      perror("socket");
+    
+    if ((socket_communication = accept(socket_connexion, (struct sockaddr *)&src, (socklen_t *)&fromlen )) == -1){
+      perror("accept");
+      close (socket_connexion);
       exit(4);
     }
     
     /* creation d'une thread qui traite la requete */
     
-    
-    
-    if  (pthread_create (&tid[i], NULL, traitement_req, communication_desc) != 0) {
+    if  (pthread_create ((pthread_t *)&tid[i], NULL, traitement_req, (void *)&socket_communication) != 0) {
       fprintf(stderr,"pthread_create a rencontre un probleme\n");
+      close (socket_connexion);
       exit(5);
     }
     
+    /* rendre la thread detachee */
+    /* la thread liberera elle meme ses ressources a la */
+    /* fin de son execution */
+    if(pthread_detach(tid[i]) != 0){
+      perror("pthread_detach");
+      close (socket_connexion);
+      exit(1);
+    }
+    /* pour stocker le tid de la prochaine thread */
     i++;
-
-
-      return 0;
   }
+  
+  
+  return 0;    
+}
+
+
