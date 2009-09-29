5af0d4f53491d58d6e5a90f7eeaf5b88da9c97d3
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2009-09-29 16:56:07 +0000

    ajout des interfaces HelloWorldENMBean et HelloWorldFRMBean. la detection des services par notre bundel est fonctionnelle

--- trunk/M2/V1/PSIA/TMEs/exemple_cours/helloworld/helloworld-fr/src/lip6/helloworld/fr/HelloWorldFR.java
+++ trunk/M2/V1/PSIA/TMEs/exemple_cours/helloworld/helloworld-fr/src/lip6/helloworld/fr/HelloWorldFR.java
@@ -3,7 +3,7 @@ package lip6.helloworld.fr;
 
 import lip6.services.helloworld.HelloWorld;
 
-public class HelloWorldFR implements HelloWorld {
+public class HelloWorldFR implements HelloWorldFRMBean {
 	public void sayHello() {
 		System.out.println("[HelloWorldFR]: Bonjour, Monde!");
 	}
