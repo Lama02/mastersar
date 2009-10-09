0c2145b68e1ae746bf81d2f9e55f1812ae6032e4
c.mouki <c.mouki@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2009-10-09 10:51:33 +0000

    Un hello world sans le MBean pour les testes.

---
+++ trunk/M2/V1/PSIA/TMEs/tme1/exemple_cours/helloworld/helloworld-es/src/lip6/helloworld/en/HelloWorldES.java
@@ -0,0 +1,10 @@
+
+package lip6.helloworld.en;
+
+import lip6.services.helloworld.HelloWorld;
+
+public class HelloWorldES implements HelloWorld {
+	public void sayHello() {
+		System.out.println("[HelloWorldES]: Hola, mundo!");
+	}
+}
