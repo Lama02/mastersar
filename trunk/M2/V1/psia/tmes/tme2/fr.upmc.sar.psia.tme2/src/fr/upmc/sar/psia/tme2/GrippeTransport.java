ba703c4dcf8992dcbf1514fe79062da7fdb07c5e
c.mouki <c.mouki@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2009-10-29 18:29:36 +0000

    prise en compte des latences (guerison et incubation). Implementation de la vaccination (pour le moment fixe a 0%).

--- trunk/M2/V1/psia/tmes/tme2/fr.upmc.sar.psia.tme2/src/fr/upmc/sar/psia/tme2/GrippeTransport.java
+++ trunk/M2/V1/psia/tmes/tme2/fr.upmc.sar.psia.tme2/src/fr/upmc/sar/psia/tme2/GrippeTransport.java
@@ -8,30 +8,52 @@ import peersim.edsim.EDSimulator;
 
 public class GrippeTransport implements Protocol {
 
-	private final long min;
-	private final long range;
+	private final long minIncubation;
+	private final long rangeIncubation;
+	
+	private final long minGuerison;
+	private final long rangeGuerison;
 
 
 	public GrippeTransport(String prefix) {
-		min = Configuration.getInt(prefix + ".mindelay");
-		long max = Configuration.getInt(prefix + ".maxdelay");
-		if (max < min) {
+		minIncubation = Configuration.getInt(prefix + ".mindelayIncubation");
+		long maxIncubation = Configuration.getInt(prefix + ".maxdelayIncubation");
+		if (maxIncubation < minIncubation) {
 			System.out.println("The maximum latency cannot be smaller than the minimum latency");
 			System.exit(1);
 		}
-		range = max-min+1;
+		rangeIncubation = maxIncubation-minIncubation+1;
+
+		minGuerison = Configuration.getInt(prefix + ".mindelayGuerison");
+		long maxGuerison = Configuration.getInt(prefix + ".maxdelayGuerison");
+		if (maxGuerison < minGuerison) {
+			System.out.println("The maximum latency cannot be smaller than the minimum latency");
+			System.exit(1);
+		}
+		rangeGuerison = maxGuerison-minGuerison+1;
 	}
 
 	// Envoi d'un message: il suffit de l'ajouter a la file d'evenements
 	public void send(Node src, Node dest, Object msg, int pid) {
-		long delay = getLatency(src,dest);
+		long delay;
+		
+		if (((Message)msg).getType() == Message.MSG_MALADE) {
+			delay = getLatencyIncubation(src, dest);
+		} else {
+			delay = getLatencyGuerison(src, dest);
+		}
+		
 		EDSimulator.add(delay, msg, dest, pid);
 	}
 
 
 	// Latence random entre la borne min et la borne max
-	public long getLatency(Node src, Node dest) {
-		return (range==1?min:min + CommonState.r.nextLong(range));
+	public long getLatencyIncubation(Node src, Node dest) {
+		return (rangeIncubation==1?minIncubation:minIncubation + CommonState.r.nextLong(rangeIncubation));
+	}
+	
+	public long getLatencyGuerison(Node src, Node dest) {
+		return (rangeGuerison==1?minGuerison:minGuerison + CommonState.r.nextLong(rangeGuerison));
 	}
 
 
