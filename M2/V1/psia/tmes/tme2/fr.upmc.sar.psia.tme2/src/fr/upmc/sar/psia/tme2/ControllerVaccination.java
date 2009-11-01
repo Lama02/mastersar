a057267194dd7e42c4c024c7f7d2fa2f2a100a3e
c.mouki <c.mouki@c31d7dd1-924f-0410-9ef4-6524c54d5c44>
2009-11-01 11:46:58 +0000

    Ajout de deux fichier pour la partie vaccination ('config_file_vacc.cfg' et 'ControllerVaccination.java'). Modification de 'Controller' comme il etait avant l'ajout du code concernant le traitement de partie vaccination. Modification des methodes 'saveMoyMalades' et 'saveMoyMorts' de la class 'Statistique'.

---
+++ M2/V1/psia/tmes/tme2/fr.upmc.sar.psia.tme2/src/fr/upmc/sar/psia/tme2/ControllerVaccination.java
@@ -0,0 +1,58 @@
+package fr.upmc.sar.psia.tme2;
+
+import peersim.config.Configuration;
+import peersim.core.Control;
+
+public class ControllerVaccination implements Control {
+
+	private Statistique statistique;
+	
+	private static int   cptExpriences = 0;
+	private static int   cptJours      = 0;
+	private static int   nbExperiences = Configuration.getInt("simulation.experiments");
+	private static int            step = Configuration.getInt("control.monmodule.step");
+	private static int         nbJours = (Configuration.getInt("simulation.endtime") + 1) / step; //TODO A Verifier pas tjs vrai
+
+	public ControllerVaccination(String prefix) {
+		statistique = Statistique.getInstance();
+		cptExpriences++;
+		cptJours = 0;
+	}
+
+	@Override
+	public boolean execute() {
+		statistique.saveNbMalades(cptJours, Grippe.nbMalade);
+		statistique.saveNbMorts(cptJours, Grippe.nbMort);
+		cptJours++;
+		if (((cptExpriences %10 ) == 0) && (cptJours >= nbJours) ) {
+			// on calcule la moyennes des malades par jour
+			statistique.saveMoyMalades();
+			// on calcule la moyennes des morts par jour
+			statistique.saveMoyMorts();
+
+			if (Configuration.getBoolean("mode_vacc") ){
+				// si le mode vaccination est active				
+				// on calcule le max des malades par poucentage de vaccination
+				statistique.saveMaxMalades();
+				// on calcule le max des morts par poucentage de vaccination
+				statistique.saveMaxMorts();
+
+				Statistique.taux_vacc += 0.01;
+				
+				// TODO reinitialiser les deux tableaux 
+				statistique.init();
+				if (cptExpriences == nbExperiences ){
+					// si on est arrive a la derniere iteration 
+					statistique.writeStatVaccinations();
+				}
+			}else{
+				// le mode vaccination n'est pas active
+				// on sauvegarde les stats dans le fichier
+				statistique.writeStat();
+			}
+			
+		}
+
+		return false;
+	}
+}
